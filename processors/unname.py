6b44e2d8b0b382de6d6709d44e585f6fe60c907b
James Walker <james.chunho+github@gmail.com>
2022-01-23 13:06:08 +0000

    Bug fixes for unname processor

--- processors/unname.py
+++ processors/unname.py
@@ -10,17 +10,21 @@ def process(graph: FSA, original_path: Path):
 
 def unname(graph: FSA) -> FSA:
     state_rename_map = {}
-    for i, state in enumerate(graph.states()):
+    state_num = 0
+    for state in graph.states():
         if state == graph.start:
-            state_num = 0
+            state_rename_map[state] = f"q0"
         else:
-            state_num = i + 1
-        state_rename_map[state] = f"q{state_num}"
+            state_num = state_num + 1
+            state_rename_map[state] = f"q{state_num}"
+    print(state_rename_map)
     unnamed_graph = FSA()
     unnamed_graph.start = state_rename_map[graph.start]
     unnamed_graph.ends = set(map(lambda s: state_rename_map[s], graph.ends))
     for tr in graph.transitions:
-        unnamed_graph.add_transition(state_rename_map[tr.start], tr.letter, state_rename_map[tr.end])
+        unnamed_graph.add_transition(
+            state_rename_map[tr.start], tr.letter, state_rename_map[tr.end]
+        )
     return unnamed_graph
 
 
